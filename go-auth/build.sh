6719e5c2ddf190bc663fc8a6c629dcc7274f3944
Mike Mueller <mike@objektarium.de>
2018-02-20 12:45:30 +0100

    changed docker hub user name

--- go-auth/build.sh
+++ go-auth/build.sh
@@ -3,6 +3,6 @@ set -e
 docker run --rm -it \
     -v $PWD:/go/src/go-messenger/go-auth/ \
     -e SOURCE_PATH=go-messenger/go-auth/ \
-    usman/go-builder:1.8
+    marsmike/go-builder:1.8
 
-docker build -t usman/go-auth .
+docker build -t marsmike/go-auth .
