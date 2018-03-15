67463894feea2258b62792c7d01a8cf53dc5b4b2
Mike Müller <MarsMike@users.noreply.github.com>
2018-03-15 08:19:34 +0100

    Fix permission problem

--- build/1.8/compile.sh
+++ build/1.8/compile.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 set -e
+
 # Set directory to where we expect code to be
 cd /go/src/${SOURCE_PATH}
+
+echo "working directory:"
+echo $PWD
+
 echo "Downloading dependencies"
 govendor sync
+
 echo "Fix formatting"
 go fmt ./...
+
 echo "Running Tests"
 go test ./... 
+
 echo "Building source"
 go build
+
+echo "Fixing permission problem"
+chown -R $HOST_UID:$HOST_GID /go/src/${SOURCE_PATH}
+
 echo "Build Successful"
